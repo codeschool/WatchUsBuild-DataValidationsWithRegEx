fa3c537fe2ab3d92104f43003102f0c6b4fbae35
dan <danbickford007@yahoo.com>
2015-07-09 15:48:52 -0400

    retrieve company logo feature

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,12 @@ class Company < ActiveRecord::Base
 
   belongs_to :registration
 
+  def logo
+    get_logo['responseData']['results'][0]["unescapedUrl"]
+  end
+
+  def get_logo
+    @get_logo ||= JSON.parse `curl "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{name}&imgsz=large&as_filetype=jpg"`
+  end
+
 end
