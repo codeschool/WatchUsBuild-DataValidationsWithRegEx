16cdb4fe77be79feb16b3dea37e6ae9c3dc563e2
dan <danbickford007@yahoo.com>
2015-07-09 17:20:39 -0400

    adds rspec, tests registration, adds logo

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 gem 'pry'
+gem 'rspec-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- app/models/company.rb
+++ app/models/company.rb
@@ -3,11 +3,24 @@ class Company < ActiveRecord::Base
   belongs_to :registration
 
   def logo
+    return '' unless available?
     get_logo['responseData']['results'][0]["unescapedUrl"]
   end
 
+  private
+
   def get_logo
-    @get_logo ||= JSON.parse `curl "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{name}&imgsz=large&as_filetype=jpg"`
+    @get_logo = begin
+      uri = URI.parse("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{name}&imgsz=large&as_filetype=jpg")
+      JSON.parse Net::HTTP.get_response(uri).body
+    end
+  end
+
+  def available?
+    get_logo and 
+    get_logo['responseData'] and 
+    get_logo['responseData']['results'] and 
+    get_logo['responseData']['results'][0]
   end
 
 end
